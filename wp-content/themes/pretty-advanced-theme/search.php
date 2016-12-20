58ce7dc91b4d327b66309023ab3a032e69c3c19c
saad <saad@koubeissi.com>
2016-12-20 23:44:41 +0000

    Added archive and search functionality

---
+++ wp-content/themes/pretty-advanced-theme/search.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+        <div class="container content">
+            <div class="main block">
+
+                <h1 class="page-header">
+                    Search Results
+                </h1>
+
+                <?php if(have_posts()) : ?>
+                    <?php while(have_posts()) : the_post(); ?>
+                        <div class="archive-post">
+                            <h4>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+                            <p>Posted On: <?php the_time('F j, Y g:i a'); ?></p>
+                        </div>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <?php echo apautop('Sorry, no posts were found'); ?>
+                <?php endif; ?>
+            </div>
+
+            <div class="side">
+                <div class="block">
+                    <h3>Sidebar Head</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis nibh non lacinia fermentum. Morbi finibus, magna in laoreet congue, velit risus vulputate dui, sed scelerisque dui neque ut velit.</p>
+                    <a class="button">More</a>
+                </div>
+            </div>
+        </div>
+<?php get_footer(); ?>
